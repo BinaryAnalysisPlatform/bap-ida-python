1aa93eb26535f55e03eddc0bda5e2f008462afa0
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-28 10:25:43 -0400

    More info in brancher output

--- plugins/bap/utils/ida.py
+++ plugins/bap/utils/ida.py
@@ -102,23 +102,42 @@ def dump_symbol_info(output_filename):
 def dump_brancher_info(output_filename):
     """Dump information for BAP's brancher into output_filename."""
     from idautils import CodeRefsFrom
-    import idc
 
     idaapi.autoWait()
 
-    def is_branch_insn(ea):
-        return len(list(CodeRefsFrom(ea, False))) > 0
-
-    def branch_list(ea):
+    def dest(ea):
         return list(CodeRefsFrom(ea, True))
+
+    def branch(ea):
+        return list(CodeRefsFrom(ea, False))
+
+    def fall(ea):
+        return list(set(dest(ea)) - set(branch(ea)))
+
+    def is_branch_insn(ea):  # Unconditional branches are also counted
+        return len(branch(ea)) > 0
+
+    def is_jump_insn(ea):  # Only unconditional branches
+        return len(branch(ea)) == len(dest(ea)) == 1
+
+    def labelled(addrs, label):
+        l = lambda d: '(0x%x %s)' % (d, label)
+        return ' '.join(map(l, addrs))
 
     with open(output_filename, 'w+') as out:
         out.write('(\n')
         for ea in all_valid_ea():
-            if is_branch_insn(ea):
-                out.write("(0x%x (%s))\n" % (
+            if is_jump_insn(ea):
+                out.write('(0x%x (%s))\n' % (
                     ea,
-                    (' '.join(map(lambda d: '0x%x' % d, branch_list(ea))))))
+                    labelled(dest(ea), 'Jump')))
+            elif is_branch_insn(ea):
+                out.write('(0x%x (%s %s))\n' % (
+                    ea,
+                    labelled(branch(ea), 'Cond'),
+                    labelled(fall(ea), 'Fall')))
+            else:
+                pass  # Normal instruction, uninteresting
         out.write(')\n')
 
 

5b4038079fceb1fb4675d34cff9bb863d4868d8c
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-28 14:46:19 -0400

    Refactor config

--- plugins/bap/utils/config.py
+++ plugins/bap/utils/config.py
@@ -6,61 +6,65 @@ import idaapi
 cfg_dir = idaapi.idadir('cfg')
 cfg_path = os.path.join(cfg_dir, 'bap.cfg')
 
-def get(key, default=None, section='default'):
-    """Get value from key:value in the config file."""
-    from bap.utils import bap_comment
+
+def _read():
     if not os.path.exists(cfg_path):
-        return default
+        return {}
+    cfg = {'default': []}
     with open(cfg_path, 'r') as f:
         current_section = 'default'
         for line in f.read().split('\n'):
             if len(line) == 0:  # Empty line
                 continue
-            elif line[0] == ';':  # Comment
-                continue
             elif line[0] == '.':  # Section
                 current_section = line[1:]
-            elif section != current_section:
-                continue
-            elif line.split()[0] == key:
-                return line.split()[1]
-    return default
+                if current_section not in cfg:
+                    cfg[current_section] = []
+            else:
+                cfg[current_section].append(line)
+    return cfg
 
-def set(key, value, section='default'):
-    """Set key:value in the config file."""
-    try:
-        with open(cfg_path, 'r') as f:
-            s = f.read()
-    except IOError:
-        s = ''
 
-    is_set = False
+def _write(cfg):
     new_config = []
-    current_section = None
-    for line in s.split('\n'):
-        if len(line) == 0:  # Empty line
+    for section in cfg:
+        new_config.append('.' + section)
+        for line in cfg[section]:
             new_config.append(line)
-            continue
-        elif current_section is None and line[0] not in ('.', ';'):
-            new_config.append('.default')
-            current_section = 'default'
-        if line[0] == ';':  # Comment
-            pass
-        elif line[0] == '.':  # Section
-            current_section = line[1:]
-        elif section != current_section:
-            continue
-        elif line.split()[0] == key:
-            line = '{}\t{}\t; Previously: {}'.format(key, value, line)
-            is_set = True
-        new_config.append(line)
-    if not is_set:
-        if section != current_section:
-            new_config.append('.' + section)
-            current_section = section
-        new_config.append('{}\t{}'.format(key, value))
-
+        new_config.append('')
     if not os.path.exists(cfg_dir):
         os.makedirs(cfg_dir)
     with open(cfg_path, 'w') as f:
         f.write('\n'.join(new_config))
+
+
+def get(key, default=None, section='default'):
+    """Get value from key:value in the config file."""
+    cfg = _read()
+    if section not in cfg:
+        return default
+    for line in cfg[section]:
+        if line[0] == ';':  # Comment
+            continue
+        elif line.split()[0] == key:
+            return line.split()[1]
+    return default
+
+
+def set(key, value, section='default'):
+    """Set key:value in the config file."""
+    cfg = _read()
+
+    if section not in cfg:
+        cfg[section] = []
+    for i, line in enumerate(cfg[section]):
+        if line[0] == ';':  # Comment
+            continue
+        elif line.split()[0] == key:
+            cfg[section][i] = '{}\t{}\t; Previously: {}'.format(
+                              key, value, line)
+            break
+    else:  # Key not previously set
+        cfg[section].append('{}\t{}'.format(key, value))
+
+    _write(cfg)

5ead27ef6534ece27e050dd6463659b3465bfacd
Oleg <forown@yandex.ru>
2019-02-28 15:24:06 -0500

    minor fix in tests

--- tests/mockidaapi.py
+++ tests/mockidaapi.py
@@ -6,12 +6,12 @@ ASKBTN_CANCEL = NotImplemented
 PLUGIN_DRAW = NotImplemented
 PLUGIN_HIDE = NotImplemented
 PLUGIN_KEEP = NotImplemented
-class plugin_t(object): NotImplemented
-class text_sink_t(object): NotImplemented
-class Choose2(object): NotImplemented
-def idadir(sub): NotImplemented
-def get_cmt(ea, off): NotImplemented
-def set_cmt(ea, off): NotImplemented
-def askyn_c(dflt, title): NotImplemented
-def get_input_file_path() : NotImplemented
-def get_segm_name(ea): NotImplemented
+class plugin_t(object): pass
+class text_sink_t(object): pass
+class Choose2(object): pass
+def idadir(sub): return NotImplemented
+def get_cmt(ea, off): return NotImplemented
+def set_cmt(ea, off): return NotImplemented
+def askyn_c(dflt, title): return NotImplemented
+def get_input_file_path() : return NotImplemented
+def get_segm_name(ea): return NotImplemented
